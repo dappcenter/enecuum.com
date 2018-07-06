bd9716bbae8e942716305dbaf5636a77dd0edc62
AwesomeNiko <my6JluH@gmail.com>
2018-07-06 17:49:36 +0300

    remove memwatch

--- io/index.js
+++ io/index.js
@@ -1,17 +1,6 @@
 const dev = require(require.resolve('dotenv')).config();
 const {app, server} = require('./server');
 require('./contractProvider');
-const memwatch = require(require.resolve('memwatch-next'));
-memwatch.on('leak', (info) => {
-  console.error('Memory leak detected:\n', info);
-  throw info;
-});
-
-memwatch.on('stats', (stats) => {
-  //console.log('memwatch::stats');
-  //console.log(stats);
-});
-
 
 server.listen(8081, '0.0.0.0');
 
