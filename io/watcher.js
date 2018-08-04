46ac175439e62145cc8fcef3e15a5e1e651f02c9
AwesomeNiko <my6JluH@gmail.com>
2018-08-04 04:32:17 +0300

    watcher update

--- io/watcher.js
+++ io/watcher.js
@@ -1,21 +1,5 @@
 const {spawn} = require('child_process');
 const {sendPureLog} = require('./informer');
 
-require('./index.js');
-
-/*function restart() {
-  let mainInstance = spawn('node', ['./index.js']);
-  mainInstance.on('close', (e) => {
-    setTimeout(() => {
-      console.log(`server was close with code: ${e}`);
-      //sendPureLog(`server was restarted with code: ${e}`);
-      restart();
-    }, 5000);
-  });
-  mainInstance.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-}*/
-
-//restart();
+require('./index.js')
 console.log('start main instance');
