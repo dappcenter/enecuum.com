b5be5e809541445dd77250c429601c45de93209b
AwesomeNiko <my6JluH@gmail.com>
2018-09-14 14:42:11 +0300

    minor fix

--- io/index.js
+++ io/index.js
@@ -1,7 +1,7 @@
 const dev = require(require.resolve('dotenv')).config();
 const {app, server} = require('./server');
-require('./social/');
-//require('./contractProvider');
+//require('./social/');
+require('./contractProvider');
 
 server.listen(8081, '0.0.0.0');
 
