9bdf54cefcf5372bd0f95228b72a622d0c61dda2
AwesomeNiko <my6JluH@gmail.com>
2018-09-27 16:09:13 +0300

    update site

--- store/actions.js
+++ store/actions.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const apiUrl = 'https://api.enecuum.com/v1';
-const pureUrl = '//enecuum.com:8081';
+const pureUrl = '//enecuum.com';
 const actions = {
   setWhiteList(store, data) {
     return new Promise(resolve => {
       axios.request({
-        url: pureUrl + '/backoffice/whitelist',
+        url: pureUrl + '/api/backoffice/whitelist',
         method: 'POST',
         data: data,
         timeout: 360 * 1000,
