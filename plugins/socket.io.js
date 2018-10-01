cf4274192fbf4190e2054aaf4384f3313b9d29bf
AwesomeNiko <my6JluH@gmail.com>
2018-10-02 02:12:05 +0300

    update socket path

--- plugins/socket.io.js
+++ plugins/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io((process.env.dev ? 'http' : 'https') + '://airdrop.enecuum.com' + (process.env.dev ? ':8081' : ''), {
+const socket = io('//', {
   path: '/io',
   reconnectionAttempts: 30,
   reconnectionDelay: 3000
