b387c4052861368c60a0cb95140ea0d6a0f84fb4
AwesomeNiko <my6JluH@gmail.com>
2018-10-02 00:54:03 +0300

    merge fix

--- io/server.js
+++ io/server.js
@@ -13,7 +13,6 @@ app.use(bodyParser.json());
 app.disable('x-powered-by');
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-<<<<<<< beta
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
