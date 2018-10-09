5d2b3b3c82a07ed3f71ce051a2f35a3c128865b9
Ivan Zhuravlev <intech@users.noreply.github.com>
2018-10-09 22:56:42 +0300

    Revert "test"

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,51 @@
 <template>
   <section class="container">
-    <!--<banner :data="mainpage.banner"></banner>-->
-    <!--<landings></landings>-->
-    <!--<ourmis></ourmis>-->
-    <!--<team></team>-->
-    <!--<roadmap :data="mainpage.roadmap"></roadmap>-->
-    <!--<partners :data="mainpage.partners"></partners>-->
+    <banner :data="mainpage.banner"></banner>
+    <landings></landings>
+    <ourmis></ourmis>
+    <team></team>
+    <roadmap :data="mainpage.roadmap"></roadmap>
+    <partners :data="mainpage.partners"></partners>
   </section>
 </template>
 
 <script>
-  // import banner from '@/components/mainpage/banner';
-  // import landings from '@/components/mainpage/landings';
-  // import ourmis from '@/components/mainpage/ourMission.vue';
-  // import roadmap from '@/components/mainpage/roadmap';
-  // import team from '@/components/mainpage/team';
-  // import partners from '@/components/mainpage/partners';
-  // import citates from '@/components/mainpage/citates';
+  import banner from '@/components/mainpage/banner';
+  import landings from '@/components/mainpage/landings';
+  import ourmis from '@/components/mainpage/ourMission.vue';
+  import roadmap from '@/components/mainpage/roadmap';
+  import team from '@/components/mainpage/team';
+  import partners from '@/components/mainpage/partners';
+  import citates from '@/components/mainpage/citates';
 
 
   import axios from 'axios';
 
   export default {
     components: {
-      // banner,
-      // landings,
-      // ourmis,
-      // roadmap,
-      // team,
-      // partners,
-      // citates
+      banner,
+      landings,
+      ourmis,
+      roadmap,
+      team,
+      partners,
+      citates
     },
     methods: {
-      // facebook() {
-      //   window.open('/oauth/facebook', '_blank', 'height=400,width=500');
-      // }
+      facebook() {
+        window.open('/oauth/facebook', '_blank', 'height=400,width=500');
+      }
     },
     mounted() {
-      // jarallax(document.querySelectorAll('.jarallax'), {
-      //   imgSize: 'contain'
-      // });
-      // jarallax(document.querySelectorAll('.dna-img'), {
-      //   imgSize: 'contain',
-      //   speed: 0.2,
-      //   type: 'scroll-opacity',
-      //   imgElement: '.dna-img img'
-      // });
+      jarallax(document.querySelectorAll('.jarallax'), {
+        imgSize: 'contain'
+      });
+      jarallax(document.querySelectorAll('.dna-img'), {
+        imgSize: 'contain',
+        speed: 0.2,
+        type: 'scroll-opacity',
+        imgElement: '.dna-img img'
+      });
     },
     async asyncData() {
       const {data} = await axios.get('/i18n/mainpage_' + 'en' + '.json');
