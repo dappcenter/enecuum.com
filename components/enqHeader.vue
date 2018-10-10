1b6795a74d2d6532e70b3475967299f9bdc1ef09
termin3000 <pro100jon@yandex.ru>
2018-10-10 03:39:21 +0300

    minor fix

--- components/enqHeader.vue
+++ components/enqHeader.vue
@@ -260,9 +260,9 @@
       if (this.$route.path === '/') {
         let meta = document.createElement('meta')
         meta.setAttribute('http-equiv', 'refresh');
-        meta.setAttribute('content', '0;URL=https://landing-dev.enecuum.com/#/');
+        meta.setAttribute('content', '0;URL=https://new.enecuum.com/');
         document.head.appendChild(meta);
-        window.location.href = "https://landing-dev.enecuum.com/";
+        window.location.href = "https://new.enecuum.com/";
       }
     },
     created() {
